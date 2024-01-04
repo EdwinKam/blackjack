9581c4ce9a6c8123ad526ac0435f1d3b27c3c36d
EdwinKam <ekam@netflix.com>
2024-01-03 17:28:31 -0800

    added cross origin

--- src/main/java/com/edwinkam/blackjack/controller/HelloController.java
+++ src/main/java/com/edwinkam/blackjack/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.edwinkam.blackjack.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @CrossOrigin
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello, World!";
+        return "Hello, World from springboot!";
     }
 }
